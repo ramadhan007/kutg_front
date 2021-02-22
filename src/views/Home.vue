2b502a89a85180b3d89e8b3d325e6990e7029a00
ramadhan007 <you@example.com>
2021-02-22 03:27:16 -0800

    deploy

---
+++ src/views/Home.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="home">
+    <!-- Header Section Begin -->
+    <Header />
+    <HerocaroKutg />
+    <BannerKutg />
+    <InstaKutg />
+    <PartnerKutg />
+    <FooterKutg />
+    <!-- Header End -->
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from "@/components/HelloWorld.vue";
+import Header from "@/components/HeaderKutg.vue";
+import HerocaroKutg from "@/components/herocaroKutg.vue";
+import BannerKutg from "@/components/BannerKutg.vue";
+import InstaKutg from "@/components/InstaKutg.vue";
+import PartnerKutg from "@/components/PartnerKutg";
+import FooterKutg from "@/components/FooterKutg";
+export default {
+  name: "Home",
+  components: {
+    Header,
+    HerocaroKutg,
+    BannerKutg,
+    InstaKutg,
+    PartnerKutg,
+    FooterKutg,
+  },
+};
+</script>
